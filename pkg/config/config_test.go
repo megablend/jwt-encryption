fcb95b2c3f3a852f668c7b60fdff4b6be1efab86
megablend <megafu.charles@yahoo.com>
2023-09-15 11:33:13 -0400

    Fix issue with mock not taking effect

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -40,18 +40,28 @@ func TestNew_shouldReturnValidDetails(t *testing.T) {
 	}
 }
 
+func TestNew_shouldReadFromDefaultFolder_whenBasePathNotSet(t *testing.T) {
+	config, configErr := New()
+
+	require.NotNil(t, config)
+	assert.NoError(t, configErr)
+	assert.EqualValues(t, "private_key.pem", config.Jwt.PrivateKey)
+	assert.EqualValues(t, "public_key.pem", config.Jwt.PublicKey)
+	assert.EqualValues(t, "config/keys", config.Jwt.Directory)
+	assert.True(t, config.Jwt.GenerateKeys)
+}
+
 func TestNew_shouldReturnError_whenUnableToReadFile(t *testing.T) {
-	t.Skip("skipping because of monkey patch comaptiblity")
 	errMsg := "test error message while opening file"
 	defer monkey.UnpatchAll()
 	patchOsOpen(errMsg)
 
 	err := os.Setenv(BASE_PATH, "/path/to/config")
 
-	config, configErr := New()
+	config := &Config{}
+	configErr := config.InitConfig()
 
 	require.NoError(t, err)
-	require.Nil(t, config)
 	assert.Error(t, configErr)
 	assert.EqualValues(t, errMsg, configErr.Error())
 }
